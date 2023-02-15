f65899b5da93913c2e59ea40ff2610373f89de95
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-02-15 02:31:08 -0300

    aula 03

---
+++ datastructure_algorithms_java/src/com/alvarenga/datastructure/vetor/teste/Aula02.java
@@ -0,0 +1,13 @@
+package com.alvarenga.datastructure.vetor.teste;
+
+import com.alvarenga.datastructure.vetor.Vetor;
+
+public class Aula02 {
+	
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+		
+	}
+
+}
