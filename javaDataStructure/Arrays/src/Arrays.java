069b2fda7b4111df92224a981a6e3e29a851683c
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-02-16 23:49:17 -0300

    Arrays 01

---
+++ javaDataStructure/Arrays/src/Arrays.java
@@ -0,0 +1,38 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        // 1D Array
+        int[] a1 = new int[8];
+        int count = 1;
+
+        for (int i = 0; i < a1.length; i++) {
+            a1[i] = count;
+            count++;
+        }
+
+        for (int j = 0; j < a1.length; j++) {
+            System.out.print(a1[j] + " ");
+        }
+
+        System.out.println();
+        // 2D Array
+
+
+        int[][] a2 = new int[3][4];
+        count = 1;
+
+        for (int i = 0; i < a2.length; i++) {
+            for (int j = 0; j < a2[i].length; j++) {
+                a2[i][j] = count;
+                count++;
+            }
+        }
+
+        for (int i = 0; i < a2.length; i++) {
+            for (int j = 0; j < a2[i].length; j++) {
+                System.out.print(a2[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
