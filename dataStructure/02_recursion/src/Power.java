eea91a78b091f2ce29ee59319ec1d1265b15cb89
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-03-14 23:37:37 -0300

    power

---
+++ dataStructure/02_recursion/src/Power.java
@@ -0,0 +1,19 @@
+public class Power {
+    public static void main(String[] args) {
+
+        Power power = new Power();
+        var result = Power.power(2, 5);
+        System.out.println(result);
+
+        result = Power.power(2, -2);
+        System.out.println(result);
+
+
+    }
+
+    public static int power(int base, int exp){
+        if(exp < 0) return -1;
+        if(exp == 0) return 1;
+        return base * power(base, exp-1);
+    }
+}
